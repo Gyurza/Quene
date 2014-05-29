943831eec0649ff0ddade0e734d0484d1766e21d
Gyurza <egorpashkevich@mail.ru>
2014-05-29 23:35:00 +0400

    console

---
+++ list_console.cpp
@@ -0,0 +1,11 @@
+#include "list_console.h"
+#include <iostream>
+
+using namespace std;
+
+Data inputData( Data data )
+{
+    cout << "Please, Enter the data: ";
+    cin >> data;
+    return data;
+}
