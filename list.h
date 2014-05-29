49fa50d401de4786285423db09b91f137342d6c6
Gyurza <egorpashkevich@mail.ru>
2014-05-29 23:33:27 +0400

    listprot

---
+++ list.h
@@ -0,0 +1,27 @@
+#ifndef LIST_H
+#define LIST_H
+#include <iostream>
+
+typedef int Data;
+
+class QueneOnSingleLinkedList
+{
+public:
+    QueneOnSingleLinkedList( );
+    ~QueneOnSingleLinkedList( );
+    void push( Data datum );
+    Data pop( );
+    void showQuene( ) const;
+    void clearQuene( );
+    bool queneIsEmpty( ) const;
+
+private:
+    struct Node
+    {
+        Data datum;
+        Node *link;
+    } *top;
+
+};
+
+#endif
